60536804a9f7ff08a205492c62d7050bdeaa0fed
Yogesh Nogia <yogeshnogia@gmail.com>
2017-02-17 18:39:22 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+var $form = $('form');
+
+var $first = $('#first');
+
+$form.on('submit', function(event){
+	var url, movie, data;
+
+	event.preventDefault();
+	movie = $form.find('input').val(); // have to pass the value first
+	url = 'http://www.omdbapi.com/?t=' + movie + '&type = movie&tomatoes = true';
+
+	$.ajax(url, {
+		complete: function(xml, status){
+			data = $.parseJSON(xml.responseText);
+
+			$first.find('.title').text(data.Title);
+			$first.find('.plot').text(data.Plot);
+
+			$first.find('.poster').html('<img src= "' + data.Poster + '" />"');
+			$first.find('.year').text(data.Year);
+			$first.show();	
+		}
+	});
+
+});
+
+
+
+
+
+
+
+
+
